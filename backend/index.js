e3927610e029275368d381e9a34ad09ccb928cde
Nikhil Yadav <132257962+nik-yadav@users.noreply.github.com>
2023-10-31 11:30:41 +0530

    index.js

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,22 @@
+const express = require('express')
+const cors = require('cors')
+const mongoCo = require('./db.js')
+mongoCo();
+const dotenv = require('dotenv')
+dotenv.config();
 
+const app = express();
+const PORT = process.env.PORT
+
+app.get('/', (req, res) => {
+    res.send("hello world")
+})
+
+app.use(cors({credentials:true, origin:'http://localhost:5173'}));
+
+app.use(express.json())
+app.use('/api',require('./Routes/CreateUser.js'))
+
+app.listen(PORT, ()=>{
+    console.log(`App is listening on port ${PORT}`);
+})
